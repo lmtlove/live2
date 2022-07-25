b9b2d8f2ee3cbc333a1ff9fe85c9ca9c7d958456
吕茂堂 <lvmaotang@lvmaotangdeMacBook-Pro.local>
2022-07-25 15:36:57 +0800

    feat:额吃啥

--- autoload.js
+++ autoload.js
@@ -13,7 +13,7 @@ _paq.push(['enableLinkTracking']);
 
 // Live2d
 // 注意：live2d_path 参数应使用绝对路径
-//const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/";
+const live2d_path = "https://github.com/lmtlove/live2.git";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
@@ -40,12 +40,12 @@ function loadExternalResource(url, type) {
 // 加载 waifu.css live2d.min.js waifu-tips.js
 if (screen.width >= 768) {
 	Promise.all([
-		loadExternalResource( "waifu.css", "css"),
-		loadExternalResource("live2d.min.js", "js"),
-		loadExternalResource( "waifu-tips.js", "js")
+		loadExternalResource(live2d_path + "waifu.css", "css"),
+		loadExternalResource(live2d_path + "live2d.min.js", "js"),
+		loadExternalResource(live2d_path + "waifu-tips.js", "js")
 	]).then(() => {
 		initWidget({
-			waifuPath: "waifu-tips.json",
+			waifuPath: live2d_path + "waifu-tips.json",
 			apiPath: "https://api.zsq.im/live2d", // https://live2d.fghrsh.net/api
 			//cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
 		});
